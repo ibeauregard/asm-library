aa65a39339e3795246970eebfb6d121c9fd8c4cb
ian beauregard <beaurega_i@qwasar.io>
2021-05-14 22:36:53 +0000

    Add strchr and test cases

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 size_t strlen_(const char* string);
+char* strchr_(const char* string, int c);
 
 int main(void)
 {
-    printf("strlen_(\"\"): %zu\nExpected: 0\n\n", strlen_(""));
-    printf("strlen_(\"strlen_ test\"): %zu\nExpected: 12\n\n", strlen_("strlen_ test"));
-    printf("strlen_(\"The quick brown fox jumps over the lazy dog\"): %zu\nExpected: 43\n\n", strlen_("The quick brown fox jumps over the lazy dog"));
+    printf("strlen_(\"\"): %zu\nExpected: 0\n\n",
+            strlen_(""));
+    printf("strlen_(\"strlen_ test\"): %zu\nExpected: 12\n\n",
+            strlen_("strlen_ test"));
+    printf("strlen_(\"The quick brown fox jumps over the lazy dog\"): %zu\nExpected: 43\n\n",
+            strlen_("The quick brown fox jumps over the lazy dog"));
+
+    printf("strchr_(\"\", 'a'): %s\nExpected: (null)\n\n",
+            strchr_("", 'a'));
+    printf("strchr_(\"The quick brown fox\", 'l'): %s\nExpected: (null)\n\n",
+            strchr_("The quick brown fox", 'l'));
+    printf("strchr_(\"The quick brown fox jumps over the lazy dog\", 'l'): \"%s\"\nExpected: \"lazy dog\"\n\n",
+            strchr_("The quick brown fox jumps over the lazy dog", 'l'));
 
     return 0;    
 }
