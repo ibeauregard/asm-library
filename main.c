cb1ffdcfa001b07d9655e67df465a3c79805b05e
ian beauregard <beaurega_i@qwasar.io>
2021-05-16 16:19:52 +0000

    Add index function and tests

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 
 size_t strlen_(const char* string);
 char* strchr_(const char* string, int c);
+char* index_(const char* string, int c);
 void* memset_(void* ptr, int value, size_t num);
 void* memcpy_(void* restrict dest, const void* restrict src, size_t num);
 int strcmp_(const char *s1, const char *s2);
@@ -37,6 +39,17 @@ int main(void)
             strchr_("The quick brown fox jumps over the lazy dog", 'l'),
             strchr("The quick brown fox jumps over the lazy dog", 'l'));
 
+    /* index */
+    printf("index_(\"\", 'a'): %p\nExpected: %p\n\n",
+            index_("", 'a'),
+            index("", 'a'));
+    printf("index_(\"The quick brown fox\", 'l'): %p\nExpected: %p\n\n",
+            index_("The quick brown fox", 'l'),
+            index("The quick brown fox", 'l'));
+    printf("index_(\"The quick brown fox jumps over the lazy dog\", 'l'): %s\nExpected: %s\n\n",
+            index_("The quick brown fox jumps over the lazy dog", 'l'),
+            index("The quick brown fox jumps over the lazy dog", 'l'));
+
     /* memset */
     char message_[] = "Almost every programmer should know memset!",
           message[] = "Almost every programmer should know memset!";
@@ -47,6 +60,7 @@ int main(void)
             (char*) memset_(message_, '-', 6),
             (char*) memset(message, '-', 6));
     
+    /* memcpy */
     printf("memcpy_(message, \"ALMOST\", 6): %s\nExpected: %s\n\n",
             (char*) memcpy_(message_, "ALMOST", 6),
             (char*) memcpy(message, "ALMOST", 6));
