0bc780893c7f2d95e4c5bc21e8e48a7f3866b151
ian beauregard <beaurega_i@qwasar.io>
2021-05-16 12:27:11 +0000

    Add strncmp and tests

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ char* strchr_(const char* string, int c);
 void* memset_(void* ptr, int value, size_t num);
 void* memcpy_(void* restrict dest, const void* restrict src, size_t num);
 int strcmp_(const char *s1, const char *s2);
+int strncmp_(const char *s1, const char *s2, size_t num);
 ssize_t read_(int fd, void* buf, size_t count);
 ssize_t write_(int fd, const void* buf, size_t count);
 
@@ -58,19 +59,28 @@ int main(void)
             strcmp_("", "a"));
     printf("strcmp_(\"The quick brown fox jumps\", \"The quick brown dog\"): %d\nExpected: <any positive number>\n\n",
             strcmp_("The quick brown fox jumps", "The quick brown dog"));
-
     printf("strcmp_(\"The quick brown dog\", \"The quick brown fox jumps\"): %d\nExpected: <any negative number>\n\n",
             strcmp_("The quick brown dog", "The quick brown fox jumps"));
+
+    printf("strncmp_(\"The quick brown dog\", \"A quick brown fox jumps\", 0): %d\nExpected: %d\n\n",
+            strncmp_("The quick brown dog", "A quick brown fox jumps", 0),
+            strncmp("The quick brown dog", "A quick brown fox jumps", 0));
+    printf("strncmp_(\"The quick brown dog\", \"The quick brown fox jumps\", 15): %d\nExpected: 0\n\n",
+            strncmp_("The quick brown dog", "The quick brown fox jumps", 15));
+    printf("strncmp_(\"The quick brown dog\", \"The quick brown fox jumps\", 19): %d\nExpected: <any negative number>\n\n",
+            strncmp_("The quick brown dog", "The quick brown fox jumps", 19));
+    printf("strncmp_(\"The quick brown fox jumps\", \"The quick brown dog\", 19): %d\nExpected: <any positive number>\n\n",
+            strncmp_("The quick brown fox jumps", "The quick brown dog", 19));
     
-    char syscall_str_[8], syscall_str[8];
+    char syscall_str_[9], syscall_str[9];
     int fd_ = open("syscall.txt", O_RDONLY), fd = open("syscall.txt", O_RDONLY);
-    printf("\nReturn value from read_(fd, syscall_str, 8): %zd\nExpected: %zd\n\n",
-            read_(fd_, syscall_str_, 8),
-            read(fd, syscall_str, 8));
+    printf("Return value from read_(fd, syscall_str, 9): %zd\nExpected: %zd\n\n",
+            read_(fd_, syscall_str_, 9),
+            read(fd, syscall_str, 9));
     close(fd_); close(fd);
-    printf("\nReturn value from write_(1, syscall_str, 8): %zd\nExpected: %zd\n\n",
-            write_(1, syscall_str, 8),
-            write(1, syscall_str, 8));
+    printf("Return value from write_(1, syscall_str, 9): %zd\nExpected: %zd\n\n",
+            write_(1, syscall_str, 9),
+            write(1, syscall_str, 9));
 
     return 0;    
 }
